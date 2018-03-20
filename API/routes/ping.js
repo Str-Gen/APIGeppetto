3aa44ae095faf04ebca22176b5bc9da8e9a72860
Str-Gen <laurens.dhooge@telenet.be>
2018-03-20 22:16:27 +0100

    Impossible to maintain a mix between ES6 & older require syntax, babel mess

--- API/routes/ping.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET ping page, heartbeat */
-router.get('/',function(req,res,next){
-    res.status(200).send("pong!");
-});
-
-module.exports = router;
